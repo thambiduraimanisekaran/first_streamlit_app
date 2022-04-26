db972f174bd2aea1ff9704d40a43c349fe860ba7
Thambidurai Manisekaran <104304064+thambiduraimanisekaran@users.noreply.github.com>
2022-04-26 12:53:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('Inside main')
+streamlit.title('My Mom's New Healthy diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
